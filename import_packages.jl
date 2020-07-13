0256ee30ccd9ac08c5d007101f7b80a4c1cedc34
Thomas Hasenzagl <6921411+thasenzagl@users.noreply.github.com>
2020-07-13 16:29:05 -0500

    Add files via upload
    install all packages at once

---
+++ import_packages.jl
@@ -0,0 +1,15 @@
+import Pkg;
+
+# Estimation
+Pkg.add("DataFrames")
+Pkg.add("Distributions")
+Pkg.add("FileIO")
+Pkg.add("BSON")
+Pkg.add("XLSX")
+
+# Charts
+Pkg.add("Colors")
+Pkg.add("CSV")
+Pkg.add("ORCA")
+Pkg.add("KernelDensity")
+Pkg.add("PlotlyJS")
